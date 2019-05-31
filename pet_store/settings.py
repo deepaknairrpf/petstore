d1627493d0136a628ce1665e6dfeb29c174c022b
Deepak <deepaknairrpf@gmail.com>
2019-05-31 12:53:20 +0530

    added procfile

--- pet_store/settings.py
+++ pet_store/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,14 +93,9 @@ WSGI_APPLICATION = 'pet_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pets',
-        'USER': 'generic_sch',
-        'PASSWORD': 'thereisnokey@123',
-        'HOST': '127.0.0.1',
-        'PORT': '13306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
 
 # Password validation
